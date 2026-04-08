a25d847c91607069b0c24e15970b7cb8b87579d5
awadridi <awad.ridi@gmail.com>
2026-04-08 16:01:14 +0300

    Update assign.py

--- assign.py
+++ assign.py
@@ -233,30 +233,30 @@ with tab_manage:
             
             st.divider()
             
-# --- دالة الرفع إلى جوجل درايف ---
-def upload_to_drive():
-    try:
-        from pydrive2.auth import GoogleAuth
-        from pydrive2.drive import GoogleDrive
-        from oauth2client.service_account import ServiceAccountCredentials
-        
-        gdata = st.secrets["gdrive_service_account"]
-        scope = ['https://www.googleapis.com/auth/drive']
-        creds = ServiceAccountCredentials.from_json_keyfile_dict(dict(gdata), scope)
-        
-        gauth = GoogleAuth()
-        gauth.credentials = creds
-        drive = GoogleDrive(gauth)
-
-        file_name = f"backup_{datetime.now().strftime('%Y-%m-%d_%H-%M')}.db"
-        folder_id = '1Aqz28edUvb9pwlD9YBgYgwQlnrpjQzrQ' 
-
-        file_drive = drive.CreateFile({'title': file_name, 'parents': [{'id': folder_id}]})
-        file_drive.SetContentFile("data_system_v26.db")
-        file_drive.Upload()
-        return True, file_name
-    except Exception as e:
-        return False, str(e)
+                # --- دالة الرفع إلى جوجل درايف ---
+                def upload_to_drive():
+                    try:
+                        from pydrive2.auth import GoogleAuth
+                        from pydrive2.drive import GoogleDrive
+                        from oauth2client.service_account import ServiceAccountCredentials
+                        
+                        gdata = st.secrets["gdrive_service_account"]
+                        scope = ['https://www.googleapis.com/auth/drive']
+                        creds = ServiceAccountCredentials.from_json_keyfile_dict(dict(gdata), scope)
+                        
+                        gauth = GoogleAuth()
+                        gauth.credentials = creds
+                        drive = GoogleDrive(gauth)
+                
+                        file_name = f"backup_{datetime.now().strftime('%Y-%m-%d_%H-%M')}.db"
+                        folder_id = '1Aqz28edUvb9pwlD9YBgYgwQlnrpjQzrQ' 
+                
+                        file_drive = drive.CreateFile({'title': file_name, 'parents': [{'id': folder_id}]})
+                        file_drive.SetContentFile("data_system_v26.db")
+                        file_drive.Upload()
+                        return True, file_name
+                    except Exception as e:
+                        return False, str(e)
 
             # ... هنا يكمل الكود السابق الخاص بأزرار الإكسل والوورد والجدول
         if h_choice:
