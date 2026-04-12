159c3db821135138df8ca1f4848bb977d087f9d6
awadridi <awad.ridi@gmail.com>
2026-04-12 17:59:04 +0300

    Update assign.py

--- assign.py
+++ assign.py
@@ -574,7 +574,7 @@ with tab_manage:
             with col_btns3:
                 output_hall_excel = io.BytesIO()
                 df_hall_excel = df_hall_details.copy()
-                df_hall_excel.insert(0, 'م', range(1, 1 + len(df_hall_excel)))
+                df_hall_excel.insert(0, 'الرقم', range(1, 1 + len(df_hall_excel)))
                 df_final_export = df_hall_excel[['الرقم', 'name', 'id', 'phone', 'school', 'role', 'city']]
                 df_final_export.columns = ['الرقم', 'الاسم الرباعي', 'رقم الهوية', 'رقم الجوال', 'المدرسة', 'المهمة', 'العنوان']
 
