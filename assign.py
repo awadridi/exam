e26cea57d4f32311b5d9c8fc3e5451ff9b20e8e2
awadridi <awad.ridi@gmail.com>
2026-04-16 08:10:43 +0300

    Update assign.py

--- assign.py
+++ assign.py
@@ -1302,7 +1302,7 @@ if st.session_state.get('system_mode') == "tasheeh":
             else:
                 st.info(f"لا يوجد تكليفات لـ {filter_subj}.")
 
-            st.divider()
+        st.divider()
 
             with col_btn2:
                 st.markdown("#### 📊 خيارات التصدير")
